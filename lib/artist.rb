c7a6ecfc2bf5d287e19760e91c34f4c72ac6be85
Daksha Patel <dakshapatel100@gmail.com>
2018-05-14 02:56:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,11 +12,14 @@ def songs
 end
 
 def add_song(song)
-  @songs << Song
+  @songs << song
+  song.artist = self
 end
 
 
-
+def add_song_by_name(song_name)
+  new_song = Song.new(song_name)
+  @new
 
 
 end
