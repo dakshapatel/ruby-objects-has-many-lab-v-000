e0b27ecad5b2cfa6b3eb071031b9565046bf6a30
Daksha Patel <dakshapatel100@gmail.com>
2018-05-14 03:33:25 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+attr_accessor :name
+
+def initialize
+  @name = name
+  @posts = []
+end
+
+def posts
+  @posts
+end
+
+def add_post(post)
+  @posts << post 
+  post.author = self
+
+
+end
